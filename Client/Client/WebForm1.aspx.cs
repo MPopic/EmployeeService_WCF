f3d7947c5587c49c3101181ec162cd06495213a2
MPopic <MPopic@users.noreply.github.com>
2020-10-14 17:31:41 +0200

    Update

---
+++ Client/Client/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetEmployee_Click(object sender, EventArgs e)
+        {
+            EmployeeService.EmployeeServiceClient client = new
+        EmployeeService.EmployeeServiceClient();
+
+            EmployeeService.Employee employee =
+                client.GetEmployee(Convert.ToInt32(txtID.Text));
+
+            txtName.Text = employee.Name;
+            txtGender.Text = employee.Gender;
+            txtDateOfBirth.Text = employee.DateOfBirth.ToShortDateString();
+
+            lblMessage.Text = "Employee retrieved";
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            EmployeeService.Employee employee = new EmployeeService.Employee();
+            employee.Id = Convert.ToInt32(txtID.Text);
+            employee.Name = txtName.Text;
+            employee.Gender = txtGender.Text;
+            employee.DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text);
+
+            EmployeeService.EmployeeServiceClient client = new
+                EmployeeService.EmployeeServiceClient();
+            client.SaveEmployee(employee);
+
+            lblMessage.Text = "Employee saved";
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtID.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtGender.Text = string.Empty;
+            txtDateOfBirth.Text = string.Empty;
+
+            lblMessage.Text = "Data cleared";
+
+        }
+    }
+}
---
+++ EmployeeService/EmployeeService/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService
+{
+    [DataContract]
+    public class Employee
+    {
+        private int _id;
+        private string _name;
+        private string _gender;
+        private DateTime _dateOfBirth;
+
+        [DataMember(Name ="ID",Order = 1)]
+        public int Id { get => _id; set => _id = value; }
+
+        [DataMember(Order = 2)]
+        public string Name { get => _name; set => _name = value; }
+
+        [DataMember(Order = 3)]
+        public string Gender { get => _gender; set => _gender = value; }
+
+        [DataMember(Order = 4)]
+        public DateTime DateOfBirth { get => _dateOfBirth; set => _dateOfBirth = value; }
+    }
+}
